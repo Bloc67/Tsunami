b6b3b0213ee5a0e77f5409799620bcc6e0dd1328
digger <digger@mysmf.net>
2019-10-01 07:16:14 +0400

    Add Russian localization

---
+++ languages/ThemeStrings.russian-utf8.php
@@ -0,0 +1,16 @@
+<?php
+
+$txt['mycss'] = 'Добавить свои стили CSS';
+$txt['mycss_desc'] = '
+';
+$txt['lastpostbindex'] = 'Скрывать последние сообщения';
+$txt['lastpostbindex2'] = 'Показывать последние сообщения';
+$txt['infobindex'] = 'Скрывать инфоцентр';
+$txt['infobindex2'] = 'Показывать инфоцентр';
+
+$txt['a_unread'] = 'Новые сообщения';
+$txt['a_replies'] = 'Новые ответы';
+$txt['a_options'] = 'Настройки';
+$txt['admin_toggle'] = 'Админка';
+
+?>
